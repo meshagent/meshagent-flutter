81a0d69977217e38b345e208a583136b814f8eae
Jesse Ezell <jesse.ezell@timu.com>
2025-04-07 10:07:48 -0700

    add room authorization options

--- lib/room_connection_scope.dart
+++ lib/room_connection_scope.dart
@@ -1,14 +1,46 @@
+import 'package:meshagent/participant_token.dart';
 import 'package:meshagent/protocol.dart';
 
 import 'package:meshagent/room_server_client.dart';
 import 'package:flutter/widgets.dart';
 
+class RoomConnectionInfo {
+  RoomConnectionInfo({required this.url, required this.jwt});
+
+  Uri url;
+  String jwt;
+}
+
+Future<RoomConnectionInfo> Function() developmentAuthorization({
+  required Uri url,
+  required String projectId,
+  required String apiKeyId,
+  required String participantName,
+  required String identity,
+  required String roomName,
+  required String secret,
+}) {
+  return () async {
+    final token = ParticipantToken(name: participantName, projectId: projectId, apiKeyId: apiKeyId);
+    token.addRoomGrant(roomName);
+    token.addRoleGrant("user");
+
+    return RoomConnectionInfo(url: url, jwt: token.toJwt(token: secret));
+  };
+}
+
+Future<RoomConnectionInfo> Function() staticAuthorization({required Uri url, required String jwt}) {
+  return () async {
+    return RoomConnectionInfo(url: url, jwt: jwt);
+  };
+}
+
 class RoomConnectionScope extends StatefulWidget {
-  const RoomConnectionScope({super.key, required this.uri, required this.builder, required this.jwt, this.doneBuilder});
+  const RoomConnectionScope({super.key, required this.authorization, required this.builder, this.doneBuilder, this.authorizingBuilder});
 
-  final Uri uri;
-  final String jwt;
+  final Future<RoomConnectionInfo> Function() authorization;
 
+  final Widget Function(BuildContext context)? authorizingBuilder;
   final Widget Function(BuildContext context, RoomClient client) builder;
   final Widget Function(BuildContext context, Object? error)? doneBuilder;
 
@@ -17,14 +49,25 @@ class RoomConnectionScope extends StatefulWidget {
 }
 
 class _RoomConnectionScopeState extends State<RoomConnectionScope> {
-  late final RoomClient client;
+  RoomClient? client;
 
   @override
   void initState() {
     super.initState();
+    connect();
+  }
 
-    client = RoomClient(protocol: Protocol(channel: WebSocketProtocolChannel(url: widget.uri, jwt: widget.jwt)));
-    client.start(onDone: onDone, onError: onError);
+  RoomConnectionInfo? connection;
+
+  void connect() async {
+    connection = await widget.authorization();
+
+    if (mounted) {
+      setState(() {
+        client = RoomClient(protocol: Protocol(channel: WebSocketProtocolChannel(url: connection!.url, jwt: connection!.jwt)));
+        client!.start(onDone: onDone, onError: onError);
+      });
+    }
   }
 
   bool done = false;
@@ -48,13 +91,20 @@ class _RoomConnectionScopeState extends State<RoomConnectionScope> {
   @override
   void dispose() {
     super.dispose();
-    client.dispose();
+    client?.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     if (!done) {
-      return widget.builder(context, client);
+      if (client == null) {
+        if (widget.authorizingBuilder != null) {
+          return widget.authorizingBuilder!(context);
+        } else {
+          return Container();
+        }
+      }
+      return widget.builder(context, client!);
     } else {
       if (widget.doneBuilder == null) {
         if (error != null) {
