a1847c607d4a4bf0ad44f3f7ae711ff905c0a218
Josef <josef.kohout@timu.com>
2025-05-23 12:21:56 -0700

    Sync call error on the first sync

--- lib/document_connection_scope.dart
+++ lib/document_connection_scope.dart
@@ -1,6 +1,8 @@
 import 'package:meshagent/room_server_client.dart';
 import 'package:flutter/widgets.dart';
 
+const int maxRetries = 3;
+
 class DocumentConnectionScope extends StatefulWidget {
   const DocumentConnectionScope({super.key, required this.room, required this.path, required this.builder});
 
@@ -15,37 +17,52 @@ class DocumentConnectionScope extends StatefulWidget {
 
 class _DocumentConnectionScope extends State<DocumentConnectionScope> {
   bool connected = false;
+  int tries = 0;
 
   late final RoomClient client;
   MeshDocument? document;
 
   Object? error;
 
+  Future<void> syncDocument() async {
+    try {
+      final doc = await widget.room.sync.open(widget.path);
+
+      if (mounted) {
+        setState(() {
+          document = doc;
+        });
+      }
+    } catch (e) {
+      if (mounted) {
+        if (tries < maxRetries) {
+          tries++;
+
+          await Future.delayed(const Duration(milliseconds: 800));
+
+          return syncDocument();
+
+        } else {
+          setState(() {
+            error = e;
+          });
+        }
+      }
+    }
+  }
+
   @override
   void initState() {
     super.initState();
 
-    widget.room.sync
-        .open(widget.path)
-        .then((doc) {
-          if (mounted) {
-            setState(() {
-              document = doc;
-            });
-          }
-        })
-        .catchError((err) {
-          if (mounted) {
-            setState(() {
-              error = err;
-            });
-          }
-        });
+    tries = 0;
+    syncDocument();
   }
 
   @override
   void dispose() {
     super.dispose();
+
     widget.room.sync.close(widget.path);
   }
 
